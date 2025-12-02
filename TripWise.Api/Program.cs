7b102681a2ffbc8c3b7c19a59cfd4cc470e3e397
Kabo <30011287@tafe.wa.edu.au>
2025-12-02 23:53:49 +0800

    connected MongoDB

--- TripWise.Api/Program.cs
+++ TripWise.Api/Program.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var conn = builder.Configuration["MongoDb:ConnectionString"];
+    return new MongoClient(conn);
+});
+
+builder.Services.AddScoped(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    var dbName = builder.Configuration["MongoDb:Database"];
+    return client.GetDatabase(dbName);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
